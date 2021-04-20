9994b00bcb707510ac8a72b1dd4e75ea85be7f64
m1cl <michael_ajala@yahoo.dejjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjjj>
2021-04-20 23:32:25 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+struct Message {}
+
+struct Info {}
+
+struct InfoList {}
+
+struct NickList {
+    name: str,
+}
+
+struct HData {}
+
+fn wee_connect() {}
+
+fn wee_handshake() {}
+
+fn wee_init() {}
+
+fn wee_sync() {}
+
+fn get_nicklist() -> NickList {}
+
+fn wee_ping() {}
+
+fn main() {
+    println!("Hello, world!");
+}
